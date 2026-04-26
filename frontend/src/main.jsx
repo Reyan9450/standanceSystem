cbaba30ba37b09add5ac6b3e4d4e1c58177efb04
Reyan9450 <reyans9450@gmail.com>
2026-04-26 16:57:53 +0530

    new frontend added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import Login from './pages/Login';
-import TeacherDashboard from './pages/TeacherDashboard';
-import StudentDashboard from './pages/StudentDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import ProtectedRoute from './components/ProtectedRoute';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/teacher" element={
-          <ProtectedRoute role="teacher"><TeacherDashboard /></ProtectedRoute>
-        } />
-        <Route path="/student" element={
-          <ProtectedRoute role="student"><StudentDashboard /></ProtectedRoute>
-        } />
-        <Route path="/admin" element={
-          <ProtectedRoute role="admin"><AdminDashboard /></ProtectedRoute>
-        } />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
