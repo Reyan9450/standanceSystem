73e507c6a187da24cd6dc6481dd126e16053f59c
Reyan9450 <reyans9450@gmail.com>
2026-04-26 17:27:38 +0530

    frotend neww

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    host: '0.0.0.0', // allow external access
+    port: 5173,      // local dev port
+  },
+
+  preview: {
+    host: '0.0.0.0', // required for Render
+    port: process.env.PORT || 4173, // Render gives PORT
+  }
 })
